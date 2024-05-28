a09e57f412ce43902d0de44c55a2229831e65a74
Viktor Kayatkin <kayatkin@mail.ru>
2024-05-28 22:47:51 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Stars from "./components/Stars/Stars";
+
+function App() {
+    return (
+      <div>
+        <h1>Movie Ratings</h1>
+        <Stars count={2}></Stars>
+      </div>
+    );
+}
+export default App
+
